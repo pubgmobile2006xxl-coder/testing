1e7298a9d0ba6023efe9a81dde90f65ddddcffe2
AZIZBEK <pubgmobile2006xxl@gmail.com>
2026-05-20 11:26:41 +0500

    cfcgvh

--- vite-project/src/Companents/Header/Header.jsx
+++ vite-project/src/Companents/Header/Header.jsx
@@ -1,4 +1,6 @@
 import second from "react";
+import grop from "../../assets/img/Group 1.png"
+import "./Header.css"
 
 function Header() {
     return(
@@ -6,10 +8,18 @@ function Header() {
         <header>
             <div className="container">
                 <div className="navar">
-                    
+                    <img src={grop} alt="rasm"/>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Work</a></li>
+                        <li><a href="#">Blog</a></li>
+                    </ul>
                 </div>
             </div>
         </header>
         </>
     )
 }
+export default Header
