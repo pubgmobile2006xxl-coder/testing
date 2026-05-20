ae5c36726af94ff5052685a97fda1ab8d2600617
AZIZBEK <pubgmobile2006xxl@gmail.com>
2026-05-20 12:34:27 +0500

    cfcgvh

--- vite-project/src/Companents/Header/Header.jsx
+++ vite-project/src/Companents/Header/Header.jsx
@@ -1,22 +1,30 @@
-import second from "react";
-import grop from "../../assets/img/Group 1.png"
-import "./Header.css"
+import React from "react";
+import { useTranslation } from "react-i18next"; // Mana shu joyi to'g'rilandi
+import grop from "../../assets/img/Group 1.png";
+import "./Header.css";
 
 function Header() {
-    return(
+    const { t, i18n } = useTranslation()
+    
+    const handleChangeLanguage = (event) => {
+        i18n.changeLanguage(event.target.value);
+    };
+
+    return (
         <>
         <header>
             <div className="container">
                 <div className="navar">
                     <img src={grop} alt="rasm"/>
                     <ul>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Services</a></li>
-                        <li><a href="#">Work</a></li>
-                        <li><a href="#">Blog</a></li>
+                        {/* Qolgan menyularni ham t() ichiga olib chiqdik */}
+                        <li><a href="#">{t("header.Home")}</a></li>
+                        <li><a href="#">{t("header.About")}</a></li>
+                        <li><a href="#">{t("header.Services")}</a></li>
+                        <li><a href="#">{t("header.Work")}</a></li>
+                        <li><a href="#">{t("header.Blog")}</a></li>
                     </ul>
-                    <select name="" id="">
+                    <select name="" id="" onChange={handleChangeLanguage} defaultValue={i18n.language}>
                         <option value="uz">uzb</option>
                         <option value="ru">rus</option>
                         <option value="en">eng</option>
@@ -25,6 +33,7 @@ function Header() {
             </div>
         </header>
         </>
-    )
+    );
 }
-export default Header
+
+export default Header;
